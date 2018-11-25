a95b8c53c887843da173f0d575e59fd9e7cf1042
Emre KURT <emre.kurt@bil.omu.edu.tr>
2018-11-25 20:39:46 +0300

    Create basic Stone class

---
+++ Stone.py
@@ -0,0 +1,22 @@
+class Stone:
+  list = []
+  def __init__(self, value, position, connections):
+    self.val = value
+    self.pos = position
+    self.connections = connections
+    Stone.list.append(self)
+
+  def __mul__(self, other):
+    if other.val in self.connections:
+      other.connections, self.connections = self.connections, other.connections
+      si = self.connections.index(self.val)
+      oi = other.connections.index(other.val)
+      self.connections[si] = other.val
+      other.connections[oi] = self.val
+
+  def __str__(self):
+    return ("value: {}, position: {}, connections: {}, manhattan distance: {}"
+    .format(self.val, self.pos, self.connections, self.distance()))
+
+  def distance(self):
+    return (self.val - self.pos)
